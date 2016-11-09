e96374f363a2e03ae74778ca8acf380226b64ec6
Ashutosh Gajankush <agajanku@stevens.edu>
2016-11-09 18:05:41 -0500

    First Commit

---
+++ ImportPDF.java
@@ -0,0 +1,19 @@
+import java.io.File; 
+import java.io.IOException;
+
+/*
+ * This class is responsible for Parsing through a PDF file and provide the following output:
+ * 1. Information of text such as font-size, font-style and color.
+ * 2. Information of Image such as what form, size and positioning of the image.
+ */
+public class ImportPDF {
+	
+	public ImportPDF(){
+		
+	}
+	//getText method will parse through the PDF file and return the text.
+	public String getText() {
+		return("Import PDF"); // Right now just returning a default text.
+	}
+	
+}
