58a666f60284dee00df501092d3d3606e9edc3f2
Aaron <ltang4@stevens.edu>
2016-11-09 20:10:35 -0500

    Lei Tang 2nd week

---
+++ src/edu/stevens/Application.java
@@ -0,0 +1,11 @@
+package edu.stevens;
+
+import javax.swing.JFrame;
+
+public class Application {
+	public static void main(String args[])
+	{	XFrame xFrame=new XFrame();
+		xFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
