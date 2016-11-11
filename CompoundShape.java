173b4ccdb383f3a755bcaff182def3f024ad777d
xiazhixuanc <xiazhixuanc@gmail.com>
2016-11-11 11:34:46 -0500

    Add files via upload

---
+++ CompoundShape.java
@@ -0,0 +1,22 @@
+package edu.stevens.XDF._2dGraphics;
+
+import java.awt.*;
+import java.util.*;
+/*
+ * According to shape information of JSON file, compound and save all shapes into array list. Draw the compound shapes..
+ * TODO: create more shape class
+ */
+public class CompoundShape {
+	private ArrayList<Shape> Compound;
+	public CompoundShape(){
+		Compound = new ArrayList<Shape>();
+		//add all SVG shapes into the array list 
+	    //Compound.add();	
+	}
+	public void paint(Graphics g){
+		for(Shape s: Compound){
+			s.paint(g);
+		}
+	}
+
+}
