0481c23abf08d61e271709108d5e1cb6a572a405
Aaron <ltang4@stevens.edu>
2016-11-10 21:43:18 -0500

    test2

--- src/edu/stevens/Test.java
+++ src/edu/stevens/Test.java
@@ -1,24 +1,16 @@
 package edu.stevens;
 
-import java.io.IOException;
+//import java.io.IOException;
 
-import com.ansj.vec.Word2VEC;
+//import com.ansj.vec.Word2VEC;
 
 public class Test {
-    public static void main(String[] args) throws IOException {
-        Word2VEC w1 = new Word2VEC() ;
-        w1.loadGoogleModel("library/corpus.bin") ;
+    public static void main(String[] args) {
+       // Word2VEC w1 = new Word2VEC() ;
+       // w1.loadGoogleModel("library/corpus.bin") ;
         
-        System.out.println(w1.distance("奥尼尔"));
-        
-        System.out.println(w1.distance("毛泽东"));
-        
-        System.out.println(w1.distance("邓小平"));
-        
-        
-        System.out.println(w1.distance("魔术队"));
-        
-        System.out.println(w1.distance("魔术"));
+        System.out.println("奥尼尔");
+       
         
     }
 }
