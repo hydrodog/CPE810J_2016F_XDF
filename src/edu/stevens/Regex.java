9ec10c5ebeddad3d1052ea4931b95eb0ae61504b
Aaron <ltang4@stevens.edu>
2016-11-11 00:18:33 -0500

    yes

---
+++ src/edu/stevens/Regex.java
@@ -0,0 +1,11 @@
+import java.util.regex.Pattern;
+
+public class Regex {
+	public static void main(String[] args){
+		String s = "this is a test (212)894231984";
+		String s2 = "stock IBM price = \"1503554123 appl";
+		//System.out.println(s.matches("[a-z]+"));
+		
+		Pattern p = Pattern.compile("\\d{3}\\\\
+	}
+}
