00e993b99086a4d0b70be351770b7a56791d28ae
xiazhixuanc <xiazhixuanc@gmail.com>
2016-11-19 00:48:46 -0500

    Update CompoundShape.java

--- 2d_graphics/CompoundShape.java
+++ 2d_graphics/CompoundShape.java
@@ -1,23 +1,36 @@
 package edu.stevens.XDF._2dGraphics;
 
-import java.awt.*;
-import java.util.*;
+import org.apache.batik.dom.svg.SVGDOMImplementation;
+import org.apache.batik.swing.JSVGCanvas;
+import org.w3c.dom.DOMImplementation;
+import org.w3c.dom.Element;
+import org.w3c.dom.svg.SVGDocument;
 /*
   @Author Yujie Ren
- * According to shape information of JSON file, compound and save all shapes into array list. Draw the compound shapes..
- * TODO: create more shape class
+ * According to shape information of parsed SVG file, compound all shapes. 
+ * Draw the compound shapes.
  */
-public class CompoundShape {
-	private ArrayList<Shape> Compound;
+public class CompoundShape extends JSVGCanvas {
+	private SVGDocument doc;
 	public CompoundShape(){
-		Compound = new ArrayList<Shape>();
-		//add all SVG shapes into the array list 
-	        //Compound.add();	
-	}
-	public void paint(Graphics g){
-		for(Shape s: Compound){
-			s.paint(g);
-		}
-	}
+		
+		DOMImplementation impl = SVGDOMImplementation.getDOMImplementation();
+		String svgNS = SVGDOMImplementation.SVG_NAMESPACE_URI;
+		doc = (SVGDocument) impl.createDocument(svgNS, "svg", null);
+		
+		// Get the root element (the SVG element)
+		Element svgRoot = doc.getDocumentElement();
+		
+		// set the width and height attribute on the svg root element
+		svgRoot.setAttributeNS(null, "width", "800");
+		svgRoot.setAttributeNS(null, "height", "800");
 
+		// Attach the shapes to the SVG root element
+		svgRoot.appendChild(new Circle("200","250","100","green","red","10",doc).paint());
+		svgRoot.appendChild(new Rect("100","100","50","80","3","2","green","red","10","0.5","0.5",doc).paint());
+		svgRoot.appendChild(new Path("M50,50 L30,50 A20,20 0 0,1 50,30 z","black","red","2",doc).paint());
+		
+		// Draw the document.
+		setSVGDocument(doc);
+	}
 }
