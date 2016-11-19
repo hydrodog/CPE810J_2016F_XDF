6e246cc21a50b532b22e3c3eb8b7f18fb37e8481
aaaahui <jzhang93@stevens.edu>
2016-11-19 01:16:46 -0500

    3rd week

--- 2d_graphics/Line.java
+++ 2d_graphics/Line.java
@@ -1,25 +1,35 @@
 package edu.stevens.XDF._2dGraphics;
 
-import java.awt.*;
-import java.awt.geom.*;
 /*
- *@author Jiahui Zhang
- *Draw line
+ * @author:Jiahui Zhang
+ * Draw SVG line
  */
-public class Line extends Shape {
-	private double LastX,LastY;
-	public Line(double x, double y,double LastX, double LastY, Color c){
-		super(x,y,c);
+
+import org.w3c.dom.Element;
+import org.w3c.dom.svg.SVGDocument;
+
+public class Line extends Shape{
+	private String LastX,LastY,strokeWidth;
+	public Line(String x,String y,String LastX, String LastY,String stroke,String strokeWidth, SVGDocument doc){
+		super(x,y,stroke,doc);
 		this.LastX = LastX;
 		this.LastY = LastY;
+		this.strokeWidth = strokeWidth;
+    }
+	
+    public Element paint() {
+		// Create the line
+		Element line = doc.createElementNS("http://www.w3.org/2000/svg", "line");
+		line.setAttributeNS(null, "x1", x);
+		line.setAttributeNS(null, "y1", y);
+		line.setAttributeNS(null, "x2", LastX);
+		line.setAttributeNS(null, "y2", LastY);
+		line.setAttributeNS(null, "stroke", color);
+		line.setAttributeNS(null, "stroke-width", strokeWidth);
+		return line;
 	}
-	@Override
-	public void paint(Graphics g) {
-		Graphics2D graph2 = (Graphics2D)g;
-		graph2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-		graph2.setColor(c);
-		graph2.draw(new Line2D.Double(x, y, LastX, LastY));
-		
-	}
-
 }
+
+
+
+	
