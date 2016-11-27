5cda4531911a92328bde2a1365accaa914e64748
Ashutosh_Gajankush <agajanku@stevens.edu>
2016-11-27 15:42:54 -0500

    Image File

--- src/edu/stevens/ConvertingToByteArray.java
+++ src/edu/stevens/ConvertingToByteArray.java
@@ -4,7 +4,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
-//import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.codec.binary.Base64;
 /*
  * Converting Image into a byte array. 
  */
@@ -17,12 +17,13 @@ public class ConvertingToByteArray {
 		byte imageData[] = new byte[(int) file.length()];
 		imageInFile.read(imageData);
 		
-		//String imageDataString = encodeImage(imageData);
+		String imageDataString = encodeImage(imageData);
+		System.out.println(imageDataString);
 	}
-	/*
+	
 	public static String encodeImage(byte[] imageByteArray){
 		return Base64.encodeBase64URLSafeString(imageByteArray);
 	}
-	*/
+	
 
 }
