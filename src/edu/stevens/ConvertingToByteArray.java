d7ba193e0a740d70921a69a86680108140f96553
Ashutosh_Gajankush <agajanku@stevens.edu>
2016-11-27 15:36:50 -0500

    Converting to Byte Array

--- src/edu/stevens/ConvertingToByteArray.java
+++ src/edu/stevens/ConvertingToByteArray.java
@@ -1,8 +1,28 @@
 package edu.stevens;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
+//import org.apache.commons.codec.binary.Base64;
 /*
  * Converting Image into a byte array. 
  */
 public class ConvertingToByteArray {
+	
+	public static void main(String[] args) throws IOException{
+		File file = new File("example.jpg");
+		
+		FileInputStream imageInFile = new FileInputStream(file);
+		byte imageData[] = new byte[(int) file.length()];
+		imageInFile.read(imageData);
+		
+		//String imageDataString = encodeImage(imageData);
+	}
+	/*
+	public static String encodeImage(byte[] imageByteArray){
+		return Base64.encodeBase64URLSafeString(imageByteArray);
+	}
+	*/
 
 }
