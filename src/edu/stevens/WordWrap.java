478b603f7ee7d612369244afe57994f7e7126f15
cpatel42 <cpatel42@stevens.edu>
2016-12-03 22:32:51 -0500

    Chintan Patel
    Putting border between text and outer window. Still need to be able to make more space in between.

--- src/edu/stevens/WordWrap.java
+++ src/edu/stevens/WordWrap.java
@@ -6,10 +6,17 @@
 package WordWrap;
 
 import javax.swing.*;
+import javax.swing.border.EtchedBorder;
+
 import java.awt.*;
 
 public class WordWrap extends JPanel {
-    public WordWrap() {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public WordWrap() {
         initializeUI();
     }
 
@@ -17,16 +24,24 @@ public class WordWrap extends JPanel {
         this.setLayout(new BorderLayout());
         this.setPreferredSize(new Dimension(400, 200));
 
-        JTextArea textArea = new JTextArea(5, 40);
-	    
-	textArea.setMargin( new Insets(20,20,20,20) );
+        JTextArea textArea = new JTextArea(600, 600);
+        
+        textArea.setMargin( new Insets(50,50,50,50) );
 
+        textArea.setOpaque(true);
+        textArea.setBackground(Color.white);
+        textArea.setBorder(
+            BorderFactory.createTitledBorder("Type Below"));
+        JTextField tfield1 = new JTextField(10);   
+        tfield1.setBorder(
+            BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(EtchedBorder.RAISED, Color.black, Color.red), "JTextField"));
+                
         String text = "I believe that we are who we choose to be. Nobody’s going to come and save you, you’ve got to save yourself. Nobody’s going to give you anything. You’ve got to go out and fight for it. Nobody knows what you want except for you. And nobody will be as sorry as you if you don’t get it. So don’t give up on your dreams.";
         
         textArea.setText(text);
         textArea.setFont(new Font("Calibri", Font.ITALIC, 18));
        
-        textArea.setLineWrap(true);       //   Implement this line in your Xframe code.
+        textArea.setLineWrap(true);      
         textArea.setWrapStyleWord(true);
         JScrollPane scrollPane = new JScrollPane(textArea);
 
