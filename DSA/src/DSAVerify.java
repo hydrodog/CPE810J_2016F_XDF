b23fcc1d36dfd4f00669c1db4aade21c45ca9380
fwangrotk <flwang86@gmail.com>
2016-12-16 06:46:22 +0800

    Update DSAVerify.java

--- DSA/src/DSAVerify.java
+++ DSA/src/DSAVerify.java
@@ -4,6 +4,7 @@ import java.security.PublicKey;
 import java.security.Signature;
 
 public class DSAVerify {     ////verify the digital signature
+	String signedinfo=null;
 	public void verify(){
 		try {
 			//load the public key and the digital signature file
@@ -20,12 +21,16 @@ public class DSAVerify {     ////verify the digital signature
 			signetcheck.initVerify(pubkey);
 			signetcheck.update(info.getBytes());
 			if (signetcheck.verify(signed)) {
-				System.out.println("signature = " + info);
+				System.out.println("signed information = " + info);
 				System.out.println("verification successful");
+				signedinfo = info;
 			} else
 				System.out.println("verification failed");
 		} catch (java.lang.Exception e) {
 			e.printStackTrace();
 		};
+	}
+	public String getInfo(){
+		return signedinfo;
 	}
 }
